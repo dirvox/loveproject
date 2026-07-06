31b1e9fdff7e741b9e2f23d210110c05da65ec7f
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-07-06 20:51:59 +0530

    ALl cod updated

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LovePurpose from './LovePurpose'
 import LovePurposeHindi from './LovePurposeHindi'
+import FlirtyLoveProposal from './FlirtyLoveProposal'
 
 const HomeMain = () => {
   return (
     <div>
         {/* <LovePurpose/> */}
-        <LovePurposeHindi/>
+        {/* <LovePurposeHindi/> */}
+        <FlirtyLoveProposal/>
     </div>
   )
 }
