cc6666e9eb8284eb9cf89df4d56bc4eefa92b3d6
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-07-06 10:30:07 +0530

    first commit

---
+++ src/components/HomeMain.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LovePurpose from './LovePurpose'
+import LovePurposeHindi from './LovePurposeHindi'
+
+const HomeMain = () => {
+  return (
+    <div>
+        {/* <LovePurpose/> */}
+        <LovePurposeHindi/>
+    </div>
+  )
+}
+
+export default HomeMain
