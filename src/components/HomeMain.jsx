3e4a9ea74ce27196f342feeb127d25719d94485e
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-07-15 13:17:40 +0530

    All code updated

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import LovePurpose from './LovePurpose'
 import LovePurposeHindi from './LovePurposeHindi'
 import FlirtyLoveProposal from './FlirtyLoveProposal'
+import BirthdaySurprise from './Birthdaysurprise'
+import InteractiveLoveStory from './InteractiveLoveStory'
+import InteractiveLoveStory2 from './InteractiveLoveStory2'
+import BirthdayWish from './BirthdayWish'
 
 const HomeMain = () => {
   return (
     <div>
         {/* <LovePurpose/> */}
         {/* <LovePurposeHindi/> */}
-        <FlirtyLoveProposal/>
+        {/* <FlirtyLoveProposal/> */}
+        {/* <BirthdaySurprise/> */}
+
+        {/* <InteractiveLoveStory/> */}
+        {/* <InteractiveLoveStory2/> */}
+        <BirthdayWish/>
     </div>
   )
 }
